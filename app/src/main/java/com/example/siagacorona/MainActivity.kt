7c9d78e3e020f5385206d6cca4e9560bf1a53c61
Surya <suryashen28@gmail.com>
2020-09-03 20:54:15 +0700

    added intent to lookup page

--- app/src/main/java/com/example/siagacorona/MainActivity.kt
+++ app/src/main/java/com/example/siagacorona/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.siagacorona
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        constraintLayout4.setOnClickListener(){
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent);
+
+        }
     }
+
 }
